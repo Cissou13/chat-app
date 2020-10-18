4e108ed43895b689b96bc9d8f928b7574129b35b
Cyrille F <cyrille.fabre@gmail.com>
2020-10-18 15:41:17 +0200

    Adding 3 Contact components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Contact from './components/Contact';
+
 function App() {
   return (
     <div className="App">
-      <Contact />,
+      <Contact userName="Peter Holt" userAvatar="https://randomuser.me/api/portraits/men/4.jpg" online={true} statusOnline={true}/>,
+      <Contact userName="Peter Robinson" userAvatar="https://randomuser.me/api/portraits/men/38.jpg" online={false}  statusOnline={false}/>,
+      <Contact userName="Sonia Patterson" userAvatar="https://randomuser.me/api/portraits/women/26.jpg"  online={true}  statusOnline={true}/>,
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import "./Contact.css";
+import PropTypes from 'prop-types';
 
+Contact.propTypes = {
+  userName : PropTypes.string.isRequired,
+  userAvatar : PropTypes.string.isRequired,
+  online : PropTypes.bool.isRequired
+};
 
-const userName = 'Peter Holt';
-const userAvatar = "https://randomuser.me/api/portraits/men/4.jpg";
-const userStatus = true;
+// const userName = 'Peter Holt';
+// const userAvatar = "https://randomuser.me/api/portraits/men/4.jpg";
+// const online = true;
+// const statusOnline = true;
 
-function Contact() {
+function Contact(props) {
   return (
     <div className="Contact">
-      <img className="avatar" src={userAvatar} />
-      <div className="name">{userName}
-      <div className="status"><div className="status-online"></div><span className="status-text">{userStatus?"online" : "offline"}</span>
+      <img className="avatar" src={props.userAvatar} />
+      <div className="name">{props.userName}
+      <div className="status">
+        <div className={props.statusOnline?"status-online":"status-offline"}></div><span className="status-text">{props.online?"online": "offline"}</span>
         </div></div>
     </div>
   );
